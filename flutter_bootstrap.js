1b42b338b2c6dac31f7cbb7b43d4a0df77f742ba
Danish <danishali9575@gmail.com>
2025-03-08 09:30:15 +0530

    instagram api added

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"871f65ac1bf129edb222c3293a636ff4b67534
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2601036141"
+    serviceWorkerVersion: "3457555874"
   }
 });
